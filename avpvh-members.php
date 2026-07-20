b84fb2e0c60af693520ebe3b914471d654bde52d
grmt <garmt.noname@gmail.com>
2026-07-21 00:27:31 +0200

    build: bump version to 1.0.8

--- avpvh-members.php
+++ avpvh-members.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: AV-PvH Members
  * Description: Member login, access control, fee tracking and admin for AV Philips van Horne.
- * Version:     1.0.7+24c5de1
+ * Version:     1.0.8+24c5de1
  * Author:      grmt
  * Text Domain: avpvh-members
  */
