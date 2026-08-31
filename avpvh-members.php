e85cada703ca19376578cde5dc39da0632dc0149
grmt <garmt.noname@gmail.com>
2026-08-31 08:14:40 +0200

    Markeer featurebuild 3c7a1f0

--- avpvh-members.php
+++ avpvh-members.php
@@ -3,7 +3,7 @@
  * Plugin Name: AV-PvH Members
  * Plugin URI:  https://github.com/grmt/avpvh-members
  * Description: Member login, access control, fee tracking and admin for AV Philips van Horne.
- * Version:     1.0.48+1ccb543
+ * Version:     1.0.48+3c7a1f0
  * Author:      grmt
  * Author URI:  https://github.com/grmt/avpvh-members
  * Text Domain: avpvh-members
